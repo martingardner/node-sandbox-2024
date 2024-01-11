49792458cefc29185176851cfad3a045c67faf20
Martin Gardner II <martingardnerii@gmail.com>
2024-01-11 13:28:09 -0600

    clean up server file

--- node-only-server/server/server.js
+++ node-only-server/server/server.js
@@ -4,31 +4,15 @@ var fs = require('fs');
 var urlSwitch = require('./url-switch')
 
 const serverSetup = () => http.createServer( (req, res) => {
-    //console.log('req', req.url);
     const urlParse = url.parse(req.url);
-    //console.log('urlParse', urlParse);
-    //console.log('urlParse.query', urlParse.query);
-    //console.log('urlParse.href', urlParse.href);
-    //console.log('res', res);
     const fileData = urlSwitch.urlSwitch(urlParse.href);
 
-    console.log('fileData', fileData);
     fs.readFile(fileData.path, (err, data) => {
         res.writeHead(200, {'Content-Type': fileData.contentType});
         res.write(data);
         return res.end();
     })
-    /*
-    fs.readFile('./htmlPages/index.html', (err, data) => {
-        //console.log('err', err);
-        //console.log('data', data);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-    */
-    
-    
+
 }).listen(8080);
 
 
