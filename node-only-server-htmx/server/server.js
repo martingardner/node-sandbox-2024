079221335f70c7fc634cf536811cf779e81780de
Martin Gardner II <martingardnerii@gmail.com>
2024-01-14 18:01:02 -0600

    move readFile to own function

--- node-only-server-htmx/server/server.js
+++ node-only-server-htmx/server/server.js
@@ -6,6 +6,23 @@ const { getQueries } = require('./crud/getQueries');
 const { postQueries } = require('./crud/postQueries');
 const { collectPostData } = require('./helpers/collectPostData');
 
+/**
+ * @function
+ * @name readFile
+ * @param {Object} fileData 
+ * @param {Object} res
+ * @description
+ * centralize the readFile logic to one place
+ */
+function readFile(fileData, res) {
+    fs.readFile(fileData.path, (err, data) => {
+        console.log('data', data);
+        res.writeHead(200, {'Content-Type': fileData.contentType});
+        res.write(data);
+        res.end();
+    })
+}
+
 
 const serverSetup = () => http.createServer( (req, res) => {
     console.log('req method', req.method);
@@ -16,25 +33,31 @@ const serverSetup = () => http.createServer( (req, res) => {
     switch(req.method.toLowerCase().trim()){
         case 'get':
             fileData = getQueries(query);
+            /*
             fs.readFile(fileData.path, (err, data) => {
                 console.log('data', data);
                 res.writeHead(200, {'Content-Type': fileData.contentType});
                 res.write(data);
                 res.end();
             })
+            */
+            readFile(fileData, res);
             break;
         case 'post':
             collectPostData(req, result => {
                 console.log('result', result);
                 fileData = postQueries(query, result);
                 console.log('POST path fileData', fileData)
+                /*
                 fs.readFile(fileData.path, (err, data) => {
                     console.log('data', data);
                     res.writeHead(200, {'Content-Type': fileData.contentType});
                     res.write(data);
                     res.end();
                 })
-            })
+                */
+                readFile(fileData, res);
+            });
             break;
         default:
             break;
